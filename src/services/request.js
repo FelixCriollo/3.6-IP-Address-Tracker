1b86b2d23c0c33739bfa5a142e96324e06bb38a1
iXiref <69726728+FelixCriollo@users.noreply.github.com>
2022-04-06 08:09:22 -0500

    Update request.js

--- src/services/request.js
+++ src/services/request.js
@@ -1,6 +1,6 @@
 const getData = async (input = "8.8.8.8") => {
   try {
-    const getData = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_BbNbH7og6ARNXxiS1v2H3WxuU8CIz&ipAddress=${input}`)
+    const getData = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${env.API}&ipAddress=${input}`)
     const data = await getData.json();
   
     return data;
